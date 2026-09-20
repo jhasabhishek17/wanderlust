572d3a427e3811ce4a06ba75984f97fffeb93bc1
Abhishek Jha <jhasabhishek17@users.noreply.github.com>
2026-09-21 02:19:46 +0530

    add listing page

---
+++ models/listing.js
@@ -0,0 +1,32 @@
+//In starting we have to require all the necessary things 
+
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const listingSchema = new Schema({
+    title: {
+        type: String,
+        required: true,
+    },
+
+    description: String,
+
+    image: {
+        filename: {
+            type: String,
+            default: "listingimage"
+        },
+        url: {
+            type: String,
+            default: "https://unsplash.com/photos/person-cooking-eggs-on-a-stovetop-following-a-recipe-PAG-wzE6R7s"
+        }
+    },
+
+    price: Number,
+    location: String,
+    country: String,
+});
+
+const Listing = mongoose.model("Listing", listingSchema);
+
+module.exports = Listing;
