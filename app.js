9481e9015c0d64c9f6d5c23571a2d19008fe9a46
Abhishek Jha <jhasabhishek17@users.noreply.github.com>
2026-09-21 18:26:34 +0530

    update code

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const mongoose = require("mongoose");
 const Listing = require("./models/listing.js") // to connect any file or folder we must require the things first 
 const path = require("path");
 const methodOverride = require("method-override");
+const ejsMate =require("ejs-mate"); 
 
 //Connection code
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
@@ -25,6 +26,8 @@ app.set("view engine","ejs");
 app.set("views",path.join(__dirname,"views"));
 app.use(express.urlencoded({extended: true}));
 app.use(methodOverride("_method"));
+app.engine('ejs',ejsMate);
+app.use(express.static(path.join(__dirname,"/public")));
 
 
 //BASIC APIs
