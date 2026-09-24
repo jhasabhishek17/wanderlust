9591679ca81339337b09a6265290e6c927517cd8
Abhishek Jha <jhasabhishek17@users.noreply.github.com>
2026-09-24 16:26:40 +0530

    update code as per new features and modules

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ const Listing = require("./models/listing.js") // to connect any file or folder 
 const path = require("path");
 const methodOverride = require("method-override");
 const ejsMate =require("ejs-mate"); 
+const wrapAsync =require("./utils/wrapAsync.js");
+const ExpressError = require("./utils/ExpressError.js");
+const {listingSchema} = require("./schema.js");
 
 //Connection code
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
@@ -36,11 +39,23 @@ app.get("/",(req,res) =>{
     res.send("Hi , I am Root");
 });
 
+const validateListing = (req,res,next) =>{
+     let {error} = listingSchema.validate(req.body);
+     if (error){
+        let errmsg = error.details.msg((el) => el.message).join(",");
+        throw new ExpressError(400,error);
+     } else {
+        next();
+     }
+
+};
+
+
 //Below code is to get the listing file (Index Route)
-app.get("/listings", async (req,res)=>{
+app.get("/listings", wrapAsync (async (req,res)=>{
    const allListings = await  Listing.find({});
    res.render("listings/index.ejs",{allListings});
-});
+}));
 
 //New Route
 app.get("/listings/new",(req,res) =>{
@@ -48,44 +63,59 @@ app.get("/listings/new",(req,res) =>{
 });
 
 //Show Route
-app.get("/listings/:id",async (req,res) =>{
+app.get("/listings/:id",wrapAsync (async (req,res) =>{
     let {id} = req.params;
     const listing = await Listing.findById(id);
     res.render("listings/show.ejs",{listing});
 
-});
+}));
 
 
 //Create Route
-app.post("/listings",async (req,res) =>{
-   const newListing = new Listing(req.body.listing);
-   await newListing.save();
-   res.redirect("/listings");
-});
+app.post("/listings",validateListing,
+    wrapAsync (async (req,res,next) =>{
+    
+     const newListing = new Listing(req.body.listing);
+     await newListing.save();
+     res.redirect("/listings");
+    })
+);
 
 //Edit Route
-app.get("/listings/:id/edit",async (req,res) =>{
+app.get("/listings/:id/edit",wrapAsync (async (req,res) =>{
     let {id} = req.params;
     const listing = await Listing.findById(id);
     res.render("listings/edit.ejs",{listing});
 
-});
+}));
 
 //Update Route
-app.put("/listings/:id",async(req,res) =>{
+app.put("/listings/:id",validateListing,wrapAsync (async(req,res) =>{
    let {id} = req.params;
    await Listing.findByIdAndUpdate(id,{...req.body.listing});
    res.redirect(`/listings/${id}`);
-});
+}));
 
 //DELETE Route
-app.delete("/listing/:id",async(req,res)=>{
+app.delete("/listing/:id",wrapAsync (async(req,res)=>{
     let {id} = req.params;
     let deletedListing = await Listing.findByIdAndDelete(id);
     console.log(deletedListing);
     res.redirect("/listings");
-})
+}));
 
+
+// "If the user requests a URL that none of my other routes handled, send a 404 error to the error-handling middleware."
+app.all("/*splat",(req,res,next) => {
+    next(new ExpressError(404,"page not found"));
+});
+
+// Error Handler
+app.use((err,req,res,next)=>{
+    let {statusCode=500,message="something wet wrong"} = err;
+    res.status(statusCode).render("error.ejs",{message});
+    //res.status(statusCode).send(message);
+});
 
 
 // Test Listing
