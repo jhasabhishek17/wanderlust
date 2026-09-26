8500a86ebb9c809a49e42b0c87c7cd1a10e3a7a4
Abhishek Jha <jhasabhishek17@users.noreply.github.com>
2026-09-26 14:09:45 +0530

    Add review js lisiting code and listing schema delete function

--- models/listing.js
+++ models/listing.js
@@ -2,6 +2,7 @@
 
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+const Review = require("./review.js")
 
 const listingSchema = new Schema({
     title: {
@@ -28,6 +29,18 @@ const listingSchema = new Schema({
     },
     location: String,
     country: String,
+    reviews:[
+        {
+            type: Schema.Types.ObjectId,
+            ref:"Review",
+        }
+    ]
+});
+
+listingSchema.post ("findOneAndDelete",async (listing) =>{
+    if(listing)
+    await Review.deleteMany({_id : {$in: listing.reviews}});
+
 });
 
 const Listing = mongoose.model("Listing", listingSchema);
